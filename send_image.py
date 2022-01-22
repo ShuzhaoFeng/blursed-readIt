fbb742aebc7a61ea8c6b8eda1fba27a406d2c855
ShuzhaoFeng <90885757+ShuzhaoFeng@users.noreply.github.com>
2022-01-22 13:45:39 -0500

    Create send_image.py

---
+++ send_image.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+from credentials import * # custom file used to store my Twilio id, auth token, Twilio phone number and my personal phone number
+
+app = Flask(__name__) # create Flask app
+
+
+@app.route('/sms', methods=['POST']) # not sure what does this do but apparently it's necessary
+def sms(): # default function for twilio
+    number = request.form.get('From') # get your phone number
+    body = request.form.get('Body') # get your input message (so your message is now stored in 'body')
+    resp = MessagingResponse() # enable response
+    my_msg = "Hello World!" # a message on top of the image
+    return client.messages.create(to=my_cell, from_=my_twilio, body=my_msg, media_url=['https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg']) # send this back to user
+
+
+if __name__ == '__main__':
+    client = Client(account_sid, auth_token) # your Twilio id and auth token
+    app.run()
