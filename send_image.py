7dab914d80dcbb1e012553402ad7a6463caddbae
ShuzhaoFeng <90885757+ShuzhaoFeng@users.noreply.github.com>
2022-01-22 13:52:27 -0500

    Update send_image.py

--- send_image.py
+++ send_image.py
@@ -11,7 +11,7 @@ def sms(): # default function for twilio
     number = request.form.get('From') # get your phone number
     body = request.form.get('Body') # get your input message (so your message is now stored in 'body')
     resp = MessagingResponse() # enable response
-    my_msg = "Hello World!" # a message on top of the image
+    my_msg = "Hello "+number+", you said "+body+"." # a short message on top of the image
     return client.messages.create(to=my_cell, from_=my_twilio, body=my_msg, media_url=['https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg']) # send this back to user
 
 
