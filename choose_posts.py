d3bce322a5e990c7f49d615235e2f7d4a668263f
Kevin Luo <91026604+Luo-Kevin@users.noreply.github.com>
2022-01-23 00:19:47 -0500

    Update choose_posts.py

--- choose_posts.py
+++ choose_posts.py
@@ -16,11 +16,11 @@ clientID = "CoNV4gzO0IjtG8lYqKuyRg"
 reddit = praw.Reddit(client_id=clientID,
                      client_secret=secret, user_agent="hackathon")
 
-text = input(">")
-
 
 def getKeywords(text):
     """
+    Get the key words from the text entered by the user
+    Returns a list of the key words
     """
     stopWords = set(stopwords.words("english"))
     sentence = nltk.word_tokenize(text)
@@ -33,6 +33,10 @@ def getKeywords(text):
 
 
 def getKeywordsNouns(filtered):
+    """
+    Classifies the word in the sentence in their respective parts of speech
+    Returns dictionary with the words in the input sentence in their respective parts of speech
+    """
     partSpeech = nltk.pos_tag(filtered)
     partsSpeechDict = {
         "Adjective": [],
@@ -40,7 +44,6 @@ def getKeywordsNouns(filtered):
         "Noun": [],
         "Interrogation": []
     }
-    print(partSpeech)
     for parts in partSpeech:
         partOfSpeech = parts[1]
         if partOfSpeech.startswith("N"):
@@ -56,11 +59,17 @@ def getKeywordsNouns(filtered):
 
 
 def sentiment_analysis(text):
+    """
+    """
     testimonial = TextBlob(text)
     return testimonial
 
 
 def analyzeTitles(titles, text):
+    """
+    Analyses the users current sentiment and finds the post that matches the most to the user's sentiment
+    Returns the reddit post
+    """
     sentimentInput = sentiment_analysis(text=text)
     userSentiment = sentimentInput.sentiment.polarity
     titleScore = dict.fromkeys(titles, 0)
@@ -84,14 +93,15 @@ def analyzeTitles(titles, text):
 
 
 def getBestPost(subs, text):
+    """
+    """
     postsTitle = []
     postsUrl = []
     for submissions in subs:
-        title = submissions.title
         url = submissions.url
-        # if url.endswith("jpeg") or url.endswith("png") or url.endswith("jpg") or url.endswith("gif"):
-        postsTitle.append(submissions.title)
-        postsUrl.append(submissions.url)
+        if url.endswith("jpeg") or url.endswith("png") or url.endswith("jpg") or url.endswith("gif"):
+            postsTitle.append(submissions.title)
+            postsUrl.append(submissions.url)
     posts = dict(zip(postsTitle, postsUrl))
     articleTitle = analyzeTitles(postsTitle, text)
     link = posts[articleTitle]
@@ -99,6 +109,9 @@ def getBestPost(subs, text):
 
 
 def getSubreddit(text):
+    """
+    Searches in the most relevant subreddit from the user's 
+    """
     keywords = getKeywords(text)
     sentenceParts = getKeywordsNouns(keywords)
     inputNouns = sentenceParts["Noun"]
@@ -116,7 +129,7 @@ def getSubreddit(text):
     try:
         return getBestPost(subs, text)
 
-    except Exception:
+    except prawcore.exceptions.NotFound:
         noun = ""
         adverb = ""
         adjective = ""
@@ -138,6 +151,13 @@ def getSubreddit(text):
 
 
 def message(text):
-    redditPost = getSubreddit(text=text)
-    stringRedditPost = f"Title: {redditPost[0]}\nURL: {redditPost[1]}"
-    return stringRedditPost
+    try:
+        redditPost = getSubreddit(text=text)
+        return redditPost[1]
+
+    except:
+        return "Our wisest and brightest team member is currently digging through the code to resolve this issue. https://images-ext-2.discordapp.net/external/rHrzbafIax5hHS5hxo2FbPLItjDeaGgFx9oP8tChCgg/%3Fwidth%3D640%26crop%3Dsmart%26auto%3Dwebp%26s%3D0ba6e5b5ccbc7677fd5d133820bbbc900d863356/https/preview.redd.it/bu98ackay7d81.jpg?width=634&height=925"
+
+
+# Enter
+# Chooses
