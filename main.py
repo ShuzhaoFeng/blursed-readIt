d3f3d9c2cd46e0fa2331d9538cee04088c9d3c38
ShuzhaoFeng <90885757+ShuzhaoFeng@users.noreply.github.com>
2022-01-23 00:10:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,12 @@
-import random
-import json
-import praw
-from textblob import TextBlob
-import nltk
-from nltk.corpus import stopwords
-from flask import Flask, request
-from twilio.rest import Client
-from twilio.twiml.messaging_response import MessagingResponse
-from credentials import *
-from difflib import get_close_matches
-
-app = Flask(__name__)  # create Flask app
-
-
-@app.route('/sms', methods=['POST'])
-def sms():  # default function for twilio
-    global text
-    text = request.form.get('Body')  # get your input message
-    resp = MessagingResponse()  # enable response
-    my_msg = "*placeholder message*"  # a message on top of the image
-    my_url = [message(text=text)[1]]
-    return client.messages.create(to=my_cell, from_=twilio_phonenum,
-                                  body=my_msg, media_url=my_url)  # send this back to user
-
-
-def getKeywords(text):
-    """
-    """
-    stopWords = set(stopwords.words("english"))
-    sentence = nltk.word_tokenize(text)
-    filteredSentence = []
-    for words in sentence:
-        if words not in stopWords:
-            filteredSentence.append(words)
-
-    return filteredSentence
-
-
-def getKeywordsNouns(filtered):
-    partSpeech = nltk.pos_tag(filtered)
-    partsSpeechDict = {
-        "Adjective": [],
-        "Adverb": [],
-        "Noun": [],
-        "Interrogation": []
-    }
-    print(partSpeech)
-    for parts in partSpeech:
-        partOfSpeech = parts[1]
-        if partOfSpeech.startswith("N"):
-            partsSpeechDict["Noun"].append(parts[0])
-        elif partOfSpeech.startswith("J"):
-            partsSpeechDict["Adjective"].append(parts[0])
-        elif partOfSpeech.startswith("R") or partOfSpeech.startswith("V"):
-            partsSpeechDict["Adverb"].append(parts[0])
-        elif partOfSpeech.startswith("W"):
-            partsSpeechDict["Interrogation"].append(parts[0])
-
-    return partsSpeechDict
-
-
-def sentiment_analysis(text):
-    testimonial = TextBlob(text)
-    return testimonial
-
-
-def analyzeTitles(titles, text):
-    sentimentInput = sentiment_analysis(text=text)
-    userSentiment = sentimentInput.sentiment.polarity
-    titleScore = dict.fromkeys(titles, 0)
-    for title in titleScore.keys():
-        processing_input = sentiment_analysis(text=title)
-        sentiment_text = processing_input.sentiment.polarity
-        titleScore[title] = sentiment_text
-
-    sortedTitleScore = {k: v for k, v in sorted(
-        titleScore.items(), key=lambda item: item[1])}
-    if userSentiment < 0:
-        sadTitle = list(sortedTitleScore.keys())[0]
-        post = sadTitle
-
-    else:
-        happyTitle = list(sortedTitleScore.keys())[-1]
-        happyUrl = sortedTitleScore[happyTitle]
-        post = happyTitle
-
-    return post
-
-
 def getBestPost(subs, text):
     postsTitle = []
     postsUrl = []
     for submissions in subs:
         title = submissions.title
         url = submissions.url
-        # if url.endswith("jpeg") or url.endswith("png") or url.endswith("jpg") or url.endswith("gif"):
-        postsTitle.append(submissions.title)
-        postsUrl.append(submissions.url)
+        if url.endswith("jpeg") or url.endswith("png") or url.endswith("jpg"):
+            postsTitle.append(submissions.title)
+            postsUrl.append(submissions.url)
     posts = dict(zip(postsTitle, postsUrl))
     articleTitle = analyzeTitles(postsTitle, text)
     link = posts[articleTitle]
@@ -144,8 +54,19 @@ def getSubreddit(text):
 
 def message(text):
     redditPost = getSubreddit(text=text)
-    stringRedditPost = f"Title: {redditPost[0]}\nURL: {redditPost[1]}"
-    return stringRedditPost
+    return redditPost[1]
+
+
+@app.route('/sms', methods=['POST'])
+def sms():  # default function for twilio
+    global text
+    text = request.form.get('Body')  # get your input message
+    resp = MessagingResponse()  # enable response
+    url = message(text)
+    my_msg = "*placeholder message*"  # a message on top of the image
+    print(str(client.messages.create(to=my_cell, from_=twilio_phonenum,
+                                  body=my_msg, media_url=[url])))
+    return ""
 
 
 if __name__ == '__main__':
