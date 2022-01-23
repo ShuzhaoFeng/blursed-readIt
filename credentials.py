c7ffcb503e596efef6517779c7ccdba72f3a3848
ShuzhaoFeng <90885757+ShuzhaoFeng@users.noreply.github.com>
2022-01-22 20:38:25 -0500

    Delete credentials.py

--- credentials.py
+++
@@ -1,6 +0,0 @@
-twilio_account_sid = 'AC3dbc90dbc1709461e1f877bcfad4c8f1'
-twilio_auth_token = 'd6b42bd47116c1520cd5895d0003bc27'
-my_cell = '+14389306806'
-twilio_phonenum = '+16067311563'
-reddit_secret = "5g48PyXKQW7J5_j5i23emTDXvgRUcg"
-reddit_clientID = "CoNV4gzO0IjtG8lYqKuyRg"
