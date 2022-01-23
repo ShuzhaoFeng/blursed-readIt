bc15d554ff63d5efa78090e22bf1b4bd6cd0df08
Kevin Luo <91026604+Luo-Kevin@users.noreply.github.com>
2022-01-22 21:25:31 -0500

    Update choose_posts.py

--- choose_posts.py
+++ choose_posts.py
@@ -6,9 +6,11 @@ from nltk.corpus import stopwords
 import nltk
 import random
 from textblob import TextBlob
+import os
 import praw
 
-
+# secret = os.environ.get("secret")
+# clientID = os.environ.get("clientID")
 secret = "5g48PyXKQW7J5_j5i23emTDXvgRUcg"
 clientID = "CoNV4gzO0IjtG8lYqKuyRg"
 reddit = praw.Reddit(client_id=clientID,
@@ -18,6 +20,8 @@ text = input("> ")
 
 
 def getKeywords(text):
+    """
+    """
     stopWords = set(stopwords.words("english"))
     sentence = nltk.word_tokenize(text)
     filteredSentence = []
@@ -83,13 +87,15 @@ def getBestPost(subs):
     postsTitle = []
     postsUrl = []
     for submissions in subs:
+        title = submissions.title
+        url = submissions.url
         # if url.endswith("jpeg") or url.endswith("png") or url.endswith("jpg") or url.endswith("gif"):
         postsTitle.append(submissions.title)
         postsUrl.append(submissions.url)
     posts = dict(zip(postsTitle, postsUrl))
     articleTitle = analyzeTitles(postsTitle, text)
     link = posts[articleTitle]
-    return [articleTitle, link]
+    return (articleTitle, link)
 
 
 def getSubreddit(text):
@@ -98,27 +104,44 @@ def getSubreddit(text):
     inputNouns = sentenceParts["Noun"]
     inputAdjective = sentenceParts["Adjective"]
     inputAdverb = sentenceParts["Adverb"]
-
-    noun = ""
-    adverb = ""
-    adjective = ""
     if len(inputNouns) != 0:
+        wordType = "noun"
         noun = random.choice(inputNouns)
-    if len(inputAdverb):
+        subs = reddit.subreddit(noun).hot(limit=100)
+    elif len(inputAdverb) != 0:
+        wordType = "adverb"
         adverb = random.choice(inputAdverb)
-    if len(inputAdjective):
+        subs = reddit.subreddit(adverb).hot(limit=100)
+    elif len(inputAdjective) != 0:
+        wordType = "adjective"
         adjective = random.choice(inputAdjective)
-    searchWord = "r/" + noun + adverb + adjective
-    print(searchWord)
-    with open("subreddits.json", "r") as data_file:
-        fp = json.load(data_file)
-        listSubreddit = list(fp)
-        suggestion = get_close_matches(
-            searchWord, listSubreddit, n=1, cutoff=0.6)
-    suggestion = suggestion[0][2:]
-    print(suggestion)
-    subs = reddit.subreddit(suggestion).hot(limit=100)
-    return getBestPost(subs)
+        subs = reddit.subreddit(adjective).hot(limit=100)
+    postsTitle = []
+    postsUrl = []
+    try:
+        return getBestPost(subs)
+
+    except Exception:
+        noun = ""
+        adverb = ""
+        adjective = ""
+        if len(inputNouns) != 0:
+            noun = random.choice(inputNouns)
+        if len(inputAdverb):
+            adverb = random.choice(inputAdverb)
+        if len(inputAdjective):
+            adjective = random.choice(inputAdjective)
+        searchWord = "r/" + noun + adverb + adjective
+        print(searchWord)
+        with open("subreddits.json", "r") as data_file:
+            fp = json.load(data_file)
+            listSubreddit = list(fp)
+            suggestion = get_close_matches(
+                searchWord, listSubreddit, n=1, cutoff=0.6)
+        suggestion = suggestion[0][2:]
+        print(suggestion)
+        subs = reddit.subreddit(suggestion).hot(limit=100)
+        return getBestPost(subs)
 
 
 def message(text):
@@ -127,4 +150,3 @@ def message(text):
 
 
 print(message(text=text))
-print(message(text=text)[1])
